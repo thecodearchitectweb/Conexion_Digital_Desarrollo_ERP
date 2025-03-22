e49af3d0cdf7bab525020def6a919e5445d8f611
Alex Alejandro Oviedo Gonzalez <thecodearchitect.web@gmail.com>
2025-03-21 23:38:46 -0500

    se avanza al 60% visual 2

--- src/controllers/modulos/incapacidades/detalle-incapacidad.controller.js
+++ src/controllers/modulos/incapacidades/detalle-incapacidad.controller.js
@@ -7,6 +7,40 @@ import express from 'express';
 const app = express();
 
 
-export const detalleIncapacidadEmpleado = (req, res) =>{
-    res.render('./views/modulos/incapacidades/detalle-incapacidad.ejs')
-}
+/* export const detalleIncapacidadEmpleado = (req, res) => {
+
+    try {
+        const { id_empleado } = req.body;
+
+        console.log(id_empleado)
+
+        res.render('./views/modulos/incapacidades/detalle-incapacidad.ejs')
+
+    } catch (error) {
+        console.error('Error al obtener detalle de incapacidad:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+
+
+} */
+
+
+    export const detalleIncapacidadEmpleado = (req, res) => {
+        try {
+            const { id } = req.params;  //  Ahora obtenemos el ID desde req.params
+    
+            if (!id) {
+                return res.status(400).send('Error: ID de empleado no proporcionado');
+            }
+    
+            console.log("ID del empleado recibido:", id);
+    
+            return res.render('./views/modulos/incapacidades/detalle-incapacidad.ejs', { id });
+    
+        } catch (error) {
+            console.error('Error al obtener detalle de incapacidad:', error);
+            return res.status(500).send('Error interno del servidor');
+        }
+    };
+    
+    
--- src/routes/modulos/incapacidades/detalle-incapacidad.routes.js
+++ src/routes/modulos/incapacidades/detalle-incapacidad.routes.js
@@ -4,7 +4,7 @@ const router = Router()
 
 import {detalleIncapacidadEmpleado} from '../../../controllers/modulos/incapacidades/detalle-incapacidad.controller.js'
 
-router.get('/incapacidad/detalle/incapacidad/empleado', detalleIncapacidadEmpleado);
+router.get('/incapacidad/detalle/incapacidad/empleado/:id', detalleIncapacidadEmpleado);
 
 
 export default router;
