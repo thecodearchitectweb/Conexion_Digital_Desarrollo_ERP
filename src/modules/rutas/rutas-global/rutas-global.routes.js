0f5daadbdc4fd2b1b1835dad04d39493c005c089
Alex Alejandro Oviedo Gonzalez <thecodearchitect.web@gmail.com>
2025-05-25 20:31:47 -0500

    se soluciona bug sobre descargar de libro excel, ruta funcional

---
+++ src/modules/rutas/rutas-global/rutas-global.routes.js
@@ -0,0 +1,16 @@
+import Login from '../../global/routes/login/login.routes.js'
+import inicio from '../../global/routes/inicio/inicio.routes.js'
+import Modulos from '../../global/routes/modulos/modulos.routes.js'
+import newUser from '../../global/routes/users/new_user.routes.js'
+import Aplicativos from '../../global/routes/aplicativos/aplicativos.routes.js'
+
+
+
+
+export default [
+    Login,
+    inicio,
+    Modulos,
+    newUser,
+    Aplicativos
+]
